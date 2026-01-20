69e3668373ad8e2142734eac8329e81403d03de3
Braian Soares Mesquita <b.mesquita.data@gmail.com>
2026-01-20 20:15:53 -0300

    Enhance table structures with additional fields
    Added new columns for product name length, description length, and photos quantity in olist_products table. Updated olist_orders table to include order approval and delivery timestamps. Added seller_id and shipping limit date in olist_order_items table.

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE olist_products (
     product_id uuid PRIMARY KEY,
     product_category_name text,
+    product_name_lenght integer,
+    product_description_lenght integer,
+    product_photos_qty integer,
     product_weight_g numeric,
     product_length_cm numeric,
     product_height_cm numeric,
@@ -11,12 +14,17 @@ CREATE TABLE olist_orders (
     customer_id uuid,
     order_status text,
     order_purchase_timestamp timestamp,
-    order_delivered_customer_date timestamp
+    order_approved_at timestamp,
+    order_delivered_carrier_date timestamp,
+    order_delivered_customer_date timestamp,
+    order_estimated_delivery_date timestamp
 );
 CREATE TABLE olist_order_items (
     order_id uuid REFERENCES olist_orders(order_id),
     order_item_id integer,
     product_id uuid REFERENCES olist_products(product_id),
+    seller_id uuid, 
+    shipping_limit_date timestamp,
     price numeric,
     freight_value numeric,
     CONSTRAINT pk_order_items PRIMARY KEY (order_id, order_item_id)
